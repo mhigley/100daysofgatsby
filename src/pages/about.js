b9a7cd271949292c90e190c073acb7bd1fce2ab9
Matthew Higley <migs@Matthews-MacBook-Pro-2.local>
2020-01-02 08:40:22 -0800

    adding layout module3 day2

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,11 @@
 import React from "react"
+import Layout from "../components/layout"
 import Header from "../components/header"
 
 export default () => (
-  <div style={{ color: `teal` }}>
+  <Layout style={{ color: `teal` }}>
     <Header headerText="About Gatsby" />
     <Header headerText="It's pretty cool" />
     <p>Such wow. Very React.</p>
-  </div>
+  </Layout>
 )
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,11 +1,15 @@
 import React from "react"
 import { Link } from "gatsby"
+import Layout from "../components/layout"
 import Header from "../components/header"
 
 export default () => (
-  <div style={{ color: `teal` }}>
+  <Layout style={{ color: `teal` }}>
     <Link to="/">Home</Link>
     <Header headerText="Contact" />
     <p>Send us a message!</p>
-  </div>
+    <p>
+      <a href="mailto:me@example.com">me@example.com</a>
+    </p>
+  </Layout>
 )
