157bcf88fa4b458c2cfddf2225f7dff06b908090
Matthew Higley <migs@Matthews-MacBook-Pro-2.local>
2020-01-03 21:27:02 -0800

    module 4

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,11 +1,12 @@
 import React from "react"
 import Layout from "../components/layout"
-import Header from "../components/header"
 
 export default () => (
-  <Layout style={{ color: `teal` }}>
-    <Header headerText="About Gatsby" />
-    <Header headerText="It's pretty cool" />
-    <p>Such wow. Very React.</p>
+  <Layout>
+    <h1>About Pandas Eating Lots</h1>
+    <p>
+      We're the only site running on your computer dedicated to showing the best
+      photos and videos of pandas eating lots of food.
+    </p>
   </Layout>
 )
