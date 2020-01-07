662c36895c4c4ef930755fab463b40e30a03e0c0
Matthew Higley <migs@Matthews-MacBook-Pro-2.local>
2020-01-07 10:40:07 -0800

    solo-ish attempt

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,12 +2,14 @@ import React from "react"
 import "../styles/styles.css"
 import Loader from "../components/loader"
 import Rocket from "../components/rocket"
+import ReactLogo from "../components/reactlogo"
 // import { ReactComponent as Logo } from "../imgs/logo.svg"
 
 export default () => {
   return (
     <div className="App">
       <header className="App-header">
+        <ReactLogo />
         <Loader />
         <Rocket />
         React + GSAP Animation
