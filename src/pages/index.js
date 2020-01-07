8f4676f26a135dcfddd3d14c71c0bd3b925ca492
Matthew Higley <migs@Matthews-MacBook-Pro-2.local>
2020-01-07 10:22:41 -0800

    incorporating gsap into gatsby

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,17 @@
 import React from "react"
+import "../styles/styles.css"
+import Loader from "../components/loader"
+import Rocket from "../components/rocket"
+// import { ReactComponent as Logo } from "../imgs/logo.svg"
 
-export default () => <div>Hello world!</div>
+export default () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Loader />
+        <Rocket />
+        React + GSAP Animation
+      </header>
+    </div>
+  )
+}
